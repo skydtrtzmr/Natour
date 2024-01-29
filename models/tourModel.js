8cb2881df27b7c39ca41d5016166ed77417f9c85
skydtrtzmr <2368178395@qq.com>
2024-01-29 17:33:03 +0800

    1-29

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 // mongoose.Schema 的参数可以分为两部分：文档定义和选项设置。
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'A tour must have a name'],
+        required: [true, 'A tour must have a name.'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal than 40 characters.'],
+        minlength: [10, 'A tour name must have more or equal than 10 characters.'],
+        // validate: [validator.isAlpha, 'Tour name must only contains characters.']
     },
     slug: {
         type: String
@@ -22,11 +26,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulity.']
+        required: [true, 'A tour must have a difficulity.'],
+        enum: {
+            values: ['easy','medium', 'difficult'], 
+            message: 'Difficulty is either: easy, medium or difficult.'
+        }
     },
     ratingsAverage: {
         type: Number,
         default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be above 5.0']
     },
     ratingsQuantity: {
         type: Number,
@@ -36,7 +46,17 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val) {
+                // this only points to current doc on NEW document creation.
+                return val< this.price;
+            },
+            message: 'Discount price ({VALUE}) should be below regular price'
+            // 这是一个mongoose特性，这里的({VALUE})可以获得被输入的值（即 priceDiscount）。
+        }
+    },
     summary: {
         type: String,
         trim: true, // 去除首尾空白
